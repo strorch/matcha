0c9d6742c15d014392d5114927632db8ca5280f3
strorch <smy980807@ukr.net>
2019-12-27 20:41:13 +0200

    minor

---
+++ tests/Infrastructure/Provider/SettingsProviderTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests\Infrastructure\Provider;
+
+use PHPUnit\Framework\TestCase;
+
+
+class SettingsProviderTest extends TestCase
+{
+    public function testAssertTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
