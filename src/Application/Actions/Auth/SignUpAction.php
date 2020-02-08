7386bce56262f7c254a2cd9793ababc3062961fe
strorch <smy980807@ukr.net>
2020-02-08 20:38:06 +0200

    removed .files

---
+++ src/Application/Actions/Auth/SignUpAction.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Application\Actions\Auth;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\StreamFactoryInterface;
+use Slim\Psr7\Request;
+
+final class RegisterAction
+{
+    /**
+     * @var StreamFactoryInterface
+     */
+    private $streamFactory;
+
+    /**
+     * GetDomainInfoAction constructor.
+     * @param StreamFactoryInterface $streamFactory
+     */
+    public function __construct(StreamFactoryInterface $streamFactory)
+    {
+    }
+
+    public function __invoke(Request $request, Response $response, array $args): Response
+    {
+        return $response
+            ->withBody($this->streamFactory->createStream('success'));
+    }
+}
