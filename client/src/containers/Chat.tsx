03e77339c86a07a13ef78e11b710ef0de69f33e3
Vladyslav Vereta <vlvereta@vlv-MacBook.local>
2020-03-07 15:44:18 +0200

    Make link redirections between first pages.

--- client/src/containers/Chat.tsx
+++ client/src/containers/Chat.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
 
-const Chat = () => <h1>Chat!</h1>;
+const Chat = () => (
+  <>
+    <h1>Chat!</h1>
+    <Link to="/">
+      <Button>Main</Button>
+    </Link>
+  </>
+);
 
 export default Chat;
--- client/src/containers/Main/index.tsx
+++ client/src/containers/Main/index.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { Link } from "react-router-dom";
+import { Button } from 'semantic-ui-react';
 import logo from 'assets/logo.svg';
 
 import './styles.css';  // FIXME: remove custom styles
@@ -7,6 +9,9 @@ const Main = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
+      <Link to="/chat">
+        <Button>Chat</Button>
+      </Link>
     </header>
   </div>
 );
