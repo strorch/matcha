acb0ae1e10245fbf96de06da9f322a4e9c7bb54d
Vladyslav Vereta <vlvereta@vlv-MacBook.local>
2020-03-23 20:57:28 +0200

    Fix links in footer.

--- client/src/containers/Footer/index.tsx
+++ client/src/containers/Footer/index.tsx
@@ -6,9 +6,9 @@ const Footer = () => (
     <Container text>
       <p className="centered-text">
         Second project on Web branch -&nbsp;
-        <strong><a href="#">Matcha</a></strong> created by&nbsp;
-        <a href="#">mstorcha</a> and <a href="#">vlvereta</a> for&nbsp;
-        <span className="no-wrap"><a href="#">UNIT_Factory</a> &copy;</span>
+        <strong><a href="https://projects.intra.42.fr/projects/matcha" target="_blank">Matcha</a></strong> created by&nbsp;
+        <a href="https://profile.intra.42.fr/users/mstorcha" target="_blank">mstorcha</a> and <a href="https://profile.intra.42.fr/users/vlvereta" target="_blank">vlvereta</a> for&nbsp;
+        <span className="no-wrap"><a href="https://unit.ua" target="_blank">UNIT_Factory</a> &copy;</span>
       </p>
     </Container>
   </footer>
