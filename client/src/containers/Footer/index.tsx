186c3d379c59a7ee4b66b64563b94fe81bf2a823
Vladyslav Vereta <vlvereta@vlv-MacBook.local>
2020-03-23 20:54:01 +0200

    Footer UI.

--- client/src/containers/Footer/index.tsx
+++ client/src/containers/Footer/index.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react';
+import { Container } from 'semantic-ui-react';
 
 const Footer = () => (
-  <footer>Hello it's a footer!</footer>
+  <footer>
+    <Container text>
+      <p className="centered-text">
+        Second project on Web branch -&nbsp;
+        <strong><a href="#">Matcha</a></strong> created by&nbsp;
+        <a href="#">mstorcha</a> and <a href="#">vlvereta</a> for&nbsp;
+        <span className="no-wrap"><a href="#">UNIT_Factory</a> &copy;</span>
+      </p>
+    </Container>
+  </footer>
 );
 
 export default Footer;
