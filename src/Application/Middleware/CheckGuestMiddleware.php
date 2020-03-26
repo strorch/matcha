74cdadd079574d6bc08c3d97aac3ab2ef87547aa
strorch <smy980807@ukr.net>
2020-03-26 02:13:11 +0200

    normalized middlewares

--- src/Application/Middleware/CheckGuestMiddleware.php
+++ src/Application/Middleware/CheckGuestMiddleware.php
@@ -7,14 +7,14 @@ namespace App\Application\Middleware;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-class CheckAuthMiddleware extends AbstractAccessMiddleware
+class CheckGuestMiddleware extends AbstractAccessMiddleware
 {
     /**
      * @inheritDoc
      */
     protected function checkAccess(ServerRequestInterface $request, RequestHandlerInterface $handler): bool
     {
-//        return !empty($this->session->get('user'));
         return true;
+//        return empty($this->session->get('user'));
     }
 }
