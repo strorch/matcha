e71c09b85ce6edd76ae9af5f1440a35c8ec1d956
strorch <smy980807@ukr.net>
2020-03-26 02:14:25 +0200

    normalized routes for new middlewares and actions

--- config/routes.php
+++ config/routes.php
@@ -3,44 +3,43 @@ declare(strict_types=1);
 
 use App\Application\Actions\Auth\LoginAction;
 use App\Application\Actions\Auth\SignUpAction;
-//use App\Socket\Client\SocketClient;
+use App\Application\Actions\Users\UsersSearchAction;
+use App\Application\Actions\Users\UserUpdateAction;
+use App\Application\Middleware\CheckGuestMiddleware;
 use Slim\App;
 use App\Application\Middleware\CheckAuthMiddleware;
 use App\Application\Middleware\JSONSerializeMiddleware;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 return static function (App $app): void {
     $c = $app->getContainer();
 
-    $app->get('/', function (Request $request, Response $response) use ($c): Response {
-
-//        $client = $c->get(SocketClient::class);
-
-//        $client->sendNotify();
-
+    $app->get('/', function (Request $request, Response $response): Response {
         $response->getBody()->write('Hello! It\'s Matcha API version 1.0!');
         return $response;
     });
 
-    $app->group('/auth', function (Group $group) {
-        $group->post('/login', LoginAction::class);
-        $group->post('/signUp', SignUpAction::class);
+    $app->group('/auth', function (Group $group) use ($c) {
+        $group->post('/login', LoginAction::class)
+            ->add(CheckGuestMiddleware::class);
+        $group->post('/signUp', SignUpAction::class)
+            ->add(CheckGuestMiddleware::class);
+        $group->post('/logout', function (Request $request, Response $response, array $params) use ($c): Response {
+                $c->get(SessionInterface::class)->clear();
+                $response->getBody()->write('success');
+                return $response;
+            })
+            ->add(CheckAuthMiddleware::class);
     });
 
     $app->group('/api', function (Group $group) {
-        $group->get('/', function (Request $request, Response $response, array $params): Response {
-            return $response;
-        });
-       $group->get('/{entities}', function (Request $request, Response $response, array $params): Response {
-           return $response;
-       });
-//        $group->get('/client', function (Request $request, Response $response, array $params): Response {
-//            return $response;
-//        });
-//        $app->get('/', \HomeController::class . ':home');
-    })
-        ->add(CheckAuthMiddleware::class)
-        ->add(JSONSerializeMiddleware::class);
+        $group->get('/users', UsersSearchAction::class);
+        $group->patch('/user',  UserUpdateAction::class);
+    })->add(CheckAuthMiddleware::class);
+
+    $app->add(JSONSerializeMiddleware::class);
 };
