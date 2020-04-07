9ea6967cdf038d3b02fb29fc164a150f19c5f669
Vladyslav Vereta <vlvereta@vlv-MacBook.local>
2020-04-07 21:02:21 +0300

    useCheckForInitialInfo hook.

--- client/src/containers/Profile/index.tsx
+++ client/src/containers/Profile/index.tsx
@@ -1,9 +1,23 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
+import { IUserState } from 'models';
+import { useCheckForInitialInfo } from 'hooks';
 
-interface IProfile {
-  temp?: string;
+interface Profile extends RouteComponentProps {
+  isInitialInfoSet: Pick<IUserState, 'isInitialInfoSet'>;
 }
 
-const Profile = ({}: IProfile) => <div />;
+const Profile = ({ isInitialInfoSet, history }: Profile) => {
+  useCheckForInitialInfo(history, isInitialInfoSet);
 
-export default Profile;
+  return <div />;
+};
+
+const mapStateToProps = state => ({
+  isInitialInfoSet: state.general.user.isInitialInfoSet
+});
+
+export default connect(
+  mapStateToProps
+)(Profile);
