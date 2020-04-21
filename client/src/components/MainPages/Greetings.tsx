561489c60db515b3ac24f1123930c4c9c96cdc5d
Vladyslav Vereta <vlvereta@vlv-MacBook.local>
2020-04-21 22:23:54 +0300

    Fix embedded  object tag.

--- client/src/components/MainPages/Greetings.tsx
+++ client/src/components/MainPages/Greetings.tsx
@@ -1,4 +1,4 @@
 import * as React from 'react';
 import greeting from 'assets/greeting.svg';
 
-export default () => <object type="image/svg+xml" data={greeting}></object>;
+export default () => <object type="image/svg+xml" data={greeting}>Greeting</object>;
