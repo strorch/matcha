b7f68b998947be135d20dc9d1e79657a3879620a
Vladyslav Vereta <vlvereta@vlv-MacBook.local>
2020-05-11 21:57:35 +0300

    Fix export formik elements.

--- client/src/components/FormikElements/index.ts
+++ client/src/components/FormikElements/index.ts
@@ -2,4 +2,5 @@ import LabeledInput from './LabeledInput';
 import LabeledSelect from './LabeledSelect';
 import LabeledTextarea from './LabeledTextarea';
 
+export { LabeledInput, LabeledSelect, LabeledTextarea };
 export default { LabeledInput, LabeledSelect, LabeledTextarea };
